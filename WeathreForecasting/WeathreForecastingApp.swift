1a190aaff842f8ee758ecfe10ea93edf3455fd7a
Sajjeel <Sajjeelh@qmic.com>
2024-10-12 12:35:03 +0300

    Initial Commit

---
+++ WeathreForecasting/WeathreForecastingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeathreForecastingApp.swift
+//  WeathreForecasting
+//
+//  Created by Sajjeel Hussain Khilji on 12/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeathreForecastingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
